a7a163f672073c081c071d47aeae81a39f38bd6e
Ferran Basora <fcsonline@gmail.com>
2019-03-05 08:10:41 +0000

    Pane working

--- tmux-thumbs.sh
+++ tmux-thumbs.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 
-/home/ubuntu/dev/tmux-thumbs/target/release/tmux-thumbs -a qwerty -r -u
+CURRENT_PANE_ID=$(tmux list-panes -F "#{pane_id}:#{?pane_active,active,nope}" | grep active | cut -d: -f1)
+COMMAND="/home/ubuntu/dev/tmux-thumbs/target/debug/tmux-thumbs -a qwerty -r -u --tmux-pane ${CURRENT_PANE_ID}"
+
+tmux new-window -n "[thumbs]" ${COMMAND}
